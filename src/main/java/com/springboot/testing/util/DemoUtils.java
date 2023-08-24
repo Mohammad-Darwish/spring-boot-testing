67002189bf5e3e0989d59247d2de812d37f4dc37
Mohammad Darwish <mohammad.darwish@ericsson.com>
2023-08-24 14:34:33 +0200

    init commit

---
+++ src/main/java/com/springboot/testing/util/DemoUtils.java
@@ -0,0 +1,5 @@
+package com.springboot.testing.util;
+
+public class DemoUtils {
+
+}
---
+++ src/test/java/com/springboot/testing/util/DemoTest.java
@@ -0,0 +1,24 @@
+package com.springboot.testing.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setUp() {
+        demoUtils = new DemoUtils();
+    }
+
+    @Test
+    void testEqualsAndNotEquals() {
+        // SET UP
+
+        // EXECUTE
+
+        // Assert
+
+    }
+}
